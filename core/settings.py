e7342b66b95a39ea632c8a3e989273277f547dd7
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-11-11 12:25:20 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -165,7 +165,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "http://starfish-app-kog2i.ondigitalocean.app",
+    
+    "https://starfish-app-kog2i.ondigitalocean.app",
 ]
 
 # Custom user model
